2188bfc7c9fe5b315da0e1590ef02b7a9f6bafd9
Michael Johns <1633607+mjohns@users.noreply.github.com>
2019-12-29 13:37:31 -0500

    symlinks

---
+++ make_symlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -sf ~/git/env/config/.tmux.conf ~/.tmux.conf
+ln -sf ~/git/env/config/.imwheelrc ~/.imwheelrc
+ln -sf ~/git/env/config/.vimrc ~/.vimrc
+ln -sf ~/git/env/config/.inputrc ~/.inputrc
+ln -sf ~/git/env/config/.ideavimrc ~/.ideavimrc
+ln -sf ~/git/env/config/.clang-format ~/.clang-format
+ln -sf ~/git/env/config/.bashrc ~/.bashrc
