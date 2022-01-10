433d7134a761d993396fc4900b7379f0d309ed0a
Michael Johns <mjohns@mjohns-home.fios-router.home>
2022-01-10 14:54:12 -0500

    Update env

--- backup.sh
+++ backup.sh
@@ -10,6 +10,9 @@ cp -f ~/bin/gls bin
 cp -f ~/bin/sort_ag.py bin
 
 cp -f ~/.vimrc config
+cp -f ~/.config/nvim/init.vim config
+cp -f ~/.xkeybindsrc config
+cp -f ~/.config/terminator/confi config/terminator_config
 cp -f ~/.inputrc config
 cp -f ~/.ideavimrc config
 cp -f ~/.imwheelrc config
